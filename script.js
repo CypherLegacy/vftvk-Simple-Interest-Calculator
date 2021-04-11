643e4fd5df063ebd287ee387291709b78e42ccc7
CypherLegacy <70846136+CypherLegacy@users.noreply.github.com>
2021-04-11 12:36:00 -0400

    Aligns .maindiv section to center, updates currency result format, adjusts td padding

--- script.js
+++ script.js
@@ -8,22 +8,32 @@ function compute() {
     var d = new Date();
     var y = d.getFullYear();
     var yr = parseInt(y) + parseInt(years);
-    
+
     //if principal is blank, 0, or negative, sends alert & focus back to principal field
     if(principal=="" || principal<=0){
         alert("Enter a positive number");
         principal.focus();
         return false;
     }
+    if(years=="-years-"){
+        alert("Select number of years");
+        years.focus();
+        return false;
+    }
     //sends the result onclick of button to return variables & calculated interest
-    var result = "If you deposit <mark>"+principal+"</mark>,<br>"+
+    document.getElementById("result").innerHTML = "If you deposit <mark>"+currency.format(principal)+"</mark>,<br>"+
     "at an interest rate of <mark>"+rate+"%</mark>.<br>"+
-    "You will receive an amount of <mark>"+interest+"</mark>,<br>"+
+    "You will receive an amount of <mark>"+currency.format(interest)+"</mark>,<br>"+
     "in the year <mark>"+yr+"</mark>.";
-    document.write(result);
 }
 
 //updates shown value next to interest rate range slider
 function updateRate(val) {
 	document.getElementById("int").value=val;
 }
+
+//formats numbers to currency with '$' and ',' with 2 decimal places with prefix "currency.format(<value>)"
+const currency = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+})
